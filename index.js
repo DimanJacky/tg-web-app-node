b17e16d53c31db5d48efb790d8023f36e1fdcc28
dmaslov <dmaslov@neoflex.ru>
2024-01-08 20:35:40 +0300

    init

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post('/web-data', async (req, res) => {
             title: 'Успешная покупка',
             input_message_content:
                 {
-                    message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products}`,
+                    message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`,
                 }
         })
         return res.status(200).json({})
